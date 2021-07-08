6643bd58c2b1b5a6936222d783b3ece2037767f2
aureiocampos <aureliocampos.dev@gmail.com>
2021-07-08 03:58:32 -0300

    Tornando dinâmica a página de quem somos

--- wp-content/themes/jotafire/template-parts/blocks/cards/horizontal-card.php
+++ wp-content/themes/jotafire/template-parts/blocks/cards/horizontal-card.php
@@ -1,14 +1,3 @@
-<section class="banner banner__primary">
-  <div class=" container banner__container banner__background">
-    <img src="#" alt="" sizes="" srcset="" />
-    <div class="banner__description">
-      <h1 class="banner__title--primary">Sua proteção contra incêndios começa agora</h1>
-      <p class="banner__subtitle--primary">Sistemas completos para evitar acidentes e riscos à vida.</p>
-      <a href="http://" class="button__primary"> Solicite um orçamento</a>
-    </div>
-  </div>
-</section>
-
 <section class="section horizontal-card__section">
   <div class="container">
     <ul class="horizontal-card__items">
@@ -28,5 +17,3 @@
     </ul>
   </div>
 </section>
-
-<?php get_template_part('components/section-cases'); ?>
